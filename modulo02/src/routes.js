5dc3039449873102734edecdcb99ebf39fc56247
Tiago <tiagosilva0922@gmail.com>
2020-02-01 19:55:29 -0300

    Adicionando criptografia na senha do usuario

--- modulo02/src/routes.js
+++ modulo02/src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Tiago',
-    email: 'tiagosilva0922@gmail.com',
-    password_hash: '123123124123',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
