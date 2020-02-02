fba15abc7fa8af32a30cdd68632caabd3503eb72
Tiago <tiagosilva0922@gmail.com>
2020-02-02 16:18:43 -0300

    Conserindo alteração de email

--- modulo02/src/app/controllers/UserController.js
+++ modulo02/src/app/controllers/UserController.js
@@ -22,7 +22,7 @@ class UserController {
     const { email, oldPassword } = req.body;
     const user = await User.findByPk(req.userId);
 
-    if (email !== user.email) {
+    if (email && email !== user.email) {
       const userExists = await User.findOne({ where: { email } });
 
       if (userExists) {
