f95f24aea0ccb636418da4ab78549b16a5e51b2c
Tiago <tiagosilva0922@gmail.com>
2020-02-12 01:02:41 -0300

    Add feature de agendamento e validações

---
+++ gobarber/src/app/controllers/AppointmentController.js
@@ -0,0 +1,62 @@
+import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
+import Appointment from '../models/Appointment';
+import User from '../models/User';
+
+class AppointmentController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      provider_id: Yup.number().required(),
+      date: Yup.date().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Falha na validação' });
+    }
+
+    const { provider_id, date } = req.body;
+    /*
+    Checkando se o provider_id, ou seja se o usuario indicado é um prestador de servico (provider=true)
+    */
+    const isProvider = await User.findOne({
+      where: { id: provider_id, provider: true },
+    });
+
+    if (!isProvider) {
+      return res.status(401).json({
+        error: 'Só pode criar agendamento com um prestador de serviço',
+      });
+    }
+    // Checkando se não é uma data antiga
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Data não permitida.' });
+    }
+
+    // Chekando se a data ja está ocupada
+    const checkAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (checkAvailability) {
+      return res
+        .status(400)
+        .json({ error: 'Data para agendamento não disponível' });
+    }
+
+    const appointment = await Appointment.create({
+      user_id: req.userId,
+      provider_id,
+      date: hourStart,
+    });
+
+    return res.json(appointment);
+  }
+}
+
+export default new AppointmentController();
