5580fed622d61715a3aad1e7a81682c59f63253b
Tiago <tiagosilva0922@gmail.com>
2020-02-15 00:32:53 -0300

    Add Nodemailer, e configurando templates de e-mail

---
+++ gobarber/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: 'da79defa87432f',
+    pass: '22b50fbaf33924',
+  },
+  default: {
+    from: 'Equipe Gobarber <noreply@gobarber.com>',
+  },
+};
