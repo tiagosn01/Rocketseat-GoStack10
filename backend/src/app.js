4ee06775762f672f2fa1230db862e0f0ec366040
tiagosn01 <tiagosilva0922@gmail.com>
2020-05-18 23:11:18 -0300

    Mudanças para buscar o avatar do usuário, e no schedule, buscar o nome

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,12 +3,11 @@ import 'dotenv/config';
 import express from 'express';
 import path from 'path';
 import cors from 'cors';
-import Youch from 'youch';
+
 import * as Sentry from '@sentry/node';
 import 'express-async-errors';
 
 import routes from './routes';
-import SentryConfig from './config/sentry';
 
 import './database';
 
@@ -16,15 +15,11 @@ class App {
   constructor() {
     this.server = express();
 
-    Sentry.init(SentryConfig);
-
     this.middlewares();
     this.routes();
-    this.exceptionHandler();
   }
 
   middlewares() {
-    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(cors());
     this.server.use(express.json());
     this.server.use(
@@ -36,18 +31,6 @@ class App {
   routes() {
     this.server.use(routes);
     this.server.use(Sentry.Handlers.errorHandler());
-  }
-
-  exceptionHandler() {
-    this.server.use(async (err, req, res, next) => {
-      if (process.env.NODE_ENV === 'development') {
-        const errors = await new Youch(err, req).toJSON();
-
-        return res.status(500).json(errors);
-      }
-
-      return res.status(500).json({ error: 'Erro interno do servidor' });
-    });
   }
 }
 
--- backend/src/app/controllers/SessionController.js
+++ backend/src/app/controllers/SessionController.js
@@ -39,13 +39,14 @@ class SessionController {
       return res.status(401).json({ error: 'Senha inválida' });
     }
 
-    const { id, name, avatar } = user;
+    const { id, name, avatar, provider } = user;
 
     return res.json({
       user: {
         id,
         name,
         email,
+        provider,
         avatar,
       },
       token: jwt.sign({ id }, authConfig.secret, {
